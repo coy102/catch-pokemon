604986418d57ebd54136c228fe7b2e2defbff064
Fajar Septiawan <fajarsep12@gmail.com>
2020-02-15 21:41:31 +0700

    changes: set layout to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import MainLayout from '@components/Layouts/Main';
 
 export default function Index() {
   return (
-    <React.Fragment>
-      <Typography variant="h6" component="h1" gutterBottom>
-        My Awesome Next JS Starter
-      </Typography>
-    </React.Fragment>
+    <MainLayout title="Awesome Pokemon">
+      <React.Fragment>
+        <Typography variant="h6" component="h1" gutterBottom>
+          My Awesome Next JS Starter
+        </Typography>
+      </React.Fragment>
+    </MainLayout>
   );
 }
