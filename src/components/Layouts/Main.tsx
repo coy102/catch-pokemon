d244657208a4573c8a4a404aafa51561e8a1e4d7
Fajar Septiawan <fajarsep12@gmail.com>
2020-02-15 21:34:33 +0700

    add: Main Layout

---
+++ src/components/Layouts/Main.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Head from 'next/head';
+import { Container, Box } from '@material-ui/core';
+
+import AppBar from '../AppBar';
+
+interface Props {
+  children: React.ReactNode;
+  title: string;
+  hasBackButton?: boolean;
+}
+
+export default function MainLayout(props: Props) {
+  const { children, title, hasBackButton } = props;
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <AppBar title={title} children={<div />} hasBackButton={hasBackButton} />
+      <Box my={10}>
+        <Container maxWidth="md">{children}</Container>
+      </Box>
+    </div>
+  );
+}
