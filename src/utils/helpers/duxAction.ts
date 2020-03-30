8b5f8eab6dd2330a809febb92065c80b575a8abb
fajarsep12 <fajarsep12@gmail.com>
2020-03-30 07:59:52 +0700

    Add perubahan 1

--- src/utils/helpers/duxAction.ts
+++ src/utils/helpers/duxAction.ts
@@ -4,5 +4,7 @@ export const generateAction = (prefix: string, action: string) =>
 export const generateActions = (prefix: string, action: string) => ({
   REQUEST: `${generateAction(prefix, action)}_REQUEST`,
   SUCCESS: `${generateAction(prefix, action)}_SUCCESS`,
-  FAILURE: `${generateAction(prefix, action)}_FAILURE`
+  FAILURE: `${generateAction(prefix, action)}_FAILURE`,
 });
+
+// Perubahan 1
